bc4dafc9fb1ca7d942ead9b045ab27832dda61ea
MateusSouzaMachado <mateus2005macgado@gmail.com>
2025-09-08 01:28:22 -0300

    first commit

---
+++ resources/js/Pages/Produtos/Index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    mensagem: String
+});
+</script>
+
+<template>
+    <Head title="Nossos Produtos" />
+
+     <div class="p-8">
+        <h1 class="text-3xl font-bold">Página de Produtos</h1>
+        <p class="mt-4 text-lg text-gray-700">
+            {{ mensagem }}
+        </p>
+    </div>
+</template>
